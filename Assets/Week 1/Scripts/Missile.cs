206c8817792a8543dc2f262ce45ebbe590583b9a
JagerBigMan <johnsondoong20040608@gmail.com>
2024-01-17 10:36:57 -0500

    Task 1 Finished

--- Assets/Week 1/Scripts/Missile.cs
+++ Assets/Week 1/Scripts/Missile.cs
@@ -1,21 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-    public float speed = 3f;
+    public float speed = 10f;
+    Rigidbody2D rigidbody;
     public GameObject missilePrefab;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = new Vector2(speed * Time.deltaTime, 0);
+        rigidbody.MovePosition(rigidbody.position + direction);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Player hit by " + collision.gameObject);
+        Destroy(gameObject);
     }
 }

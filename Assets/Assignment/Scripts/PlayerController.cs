132a5d6fc61423b08efdd90aa1bd648603fb0f62
JagerBigMan <johnsondoong20040608@gmail.com>
2024-01-29 19:41:22 -0500

    Added a reference to the rigidbody2D component

--- Assets/Assignment/Scripts/PlayerController.cs
+++ Assets/Assignment/Scripts/PlayerController.cs
@@ -4,6 +4,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    // Reference to the Rigidbody2D component
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        // Get the Rigidbody2D component on the player
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         // Get horizontal input
@@ -15,6 +24,12 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer(horizontalInput, verticalInput);
     }
 
+    void MovePlayer(float horizontalInput, float verticalInput)
+    {
+        // Calculate movement direction
+        Vector2 moveDirection = new Vector2(horizontalInput, verticalInput);
 
-
+        // Apply force to the player based on input
+        rb.AddForce(moveDirection * moveSpeed);
+    }
 }
